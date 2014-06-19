bd6d3e486b38e0c5dba58a3f10722b5cd8473105
hjp4lucifer <hjp_0002@163.com>
2014-06-19 21:47:37 +0800

    6-19超总倍机更新

--- chrome/lucifer_sdop.js
+++ chrome/lucifer_sdop.js
@@ -770,8 +770,8 @@ lcf.sdop.boss = {
 	kind: ["NORMAL", "SUPER"],
 	currentMode: 'RAID_BOSS',
 	currentKind: 'SUPER',
-	x3: 250065,
-	x6: 250066,
+	x3: 250067,
+	x6: 250068,
 	getTopLevel: function(list){
 		var target, _currentBoss;
 		var Least_Hp = 5000000;
