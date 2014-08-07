669ab532fc1a4db9257aab913710ee3f6d04f7d5
hjp4lucifer <hjp_0002@163.com>
2014-08-07 20:15:17 +0800

    2014-08-07超总倍机预测

--- chrome/lucifer_sdop.js
+++ chrome/lucifer_sdop.js
@@ -771,8 +771,8 @@ lcf.sdop.boss = {
 	kind: ["NORMAL", "SUPER"],
 	currentMode: 'RAID_BOSS',
 	currentKind: 'SUPER',
-	x3: [250072],
-	x6: 250073,
+	x3: [250074],
+	x6: 250075,
 	getTopLevel: function(list){
 		var target, _currentBoss;
 		var Least_Hp = 5000000;
